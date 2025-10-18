a2ad86557033b2eae0300971344876528fb3073f
walimajid644 <147191282+walimajid644@users.noreply.github.com>
2025-10-18 15:00:30 +0500

    Updated Vite config for GitHub Pages deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import "./main.css"
 
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+ <HashRouter>
  <App/>
- </BrowserRouter>
+ </HashRouter>
 );
  
